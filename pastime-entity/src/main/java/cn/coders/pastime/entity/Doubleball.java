484890d027495fa13d7c1009eb19191a7ca274c4
shaorensheng <329917086@qq.com>
2018-08-23 19:59:56 +0800

    增加实体类

---
+++ pastime-entity/src/main/java/cn/coders/pastime/entity/Doubleball.java
@@ -0,0 +1,193 @@
+package cn.coders.pastime.entity;
+
+public class Doubleball {
+    private Integer id;
+
+    private Integer red1;
+
+    private Integer red2;
+
+    private Integer red3;
+
+    private Integer red4;
+
+    private Integer red5;
+
+    private Integer red6;
+
+    private Integer blue;
+
+    private Long sales;
+
+    private Double accumulatedsales;
+
+    private Long onenum;
+
+    private Long onebonus;
+
+    private Long twonum;
+
+    private Long twobonus;
+
+    private Long threenum;
+
+    private Long fournum;
+
+    private Long fivenum;
+
+    private Long sixnum;
+
+    private Integer year;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getRed1() {
+        return red1;
+    }
+
+    public void setRed1(Integer red1) {
+        this.red1 = red1;
+    }
+
+    public Integer getRed2() {
+        return red2;
+    }
+
+    public void setRed2(Integer red2) {
+        this.red2 = red2;
+    }
+
+    public Integer getRed3() {
+        return red3;
+    }
+
+    public void setRed3(Integer red3) {
+        this.red3 = red3;
+    }
+
+    public Integer getRed4() {
+        return red4;
+    }
+
+    public void setRed4(Integer red4) {
+        this.red4 = red4;
+    }
+
+    public Integer getRed5() {
+        return red5;
+    }
+
+    public void setRed5(Integer red5) {
+        this.red5 = red5;
+    }
+
+    public Integer getRed6() {
+        return red6;
+    }
+
+    public void setRed6(Integer red6) {
+        this.red6 = red6;
+    }
+
+    public Integer getBlue() {
+        return blue;
+    }
+
+    public void setBlue(Integer blue) {
+        this.blue = blue;
+    }
+
+    public Long getSales() {
+        return sales;
+    }
+
+    public void setSales(Long sales) {
+        this.sales = sales;
+    }
+
+    public Double getAccumulatedsales() {
+        return accumulatedsales;
+    }
+
+    public void setAccumulatedsales(Double accumulatedsales) {
+        this.accumulatedsales = accumulatedsales;
+    }
+
+    public Long getOnenum() {
+        return onenum;
+    }
+
+    public void setOnenum(Long onenum) {
+        this.onenum = onenum;
+    }
+
+    public Long getOnebonus() {
+        return onebonus;
+    }
+
+    public void setOnebonus(Long onebonus) {
+        this.onebonus = onebonus;
+    }
+
+    public Long getTwonum() {
+        return twonum;
+    }
+
+    public void setTwonum(Long twonum) {
+        this.twonum = twonum;
+    }
+
+    public Long getTwobonus() {
+        return twobonus;
+    }
+
+    public void setTwobonus(Long twobonus) {
+        this.twobonus = twobonus;
+    }
+
+    public Long getThreenum() {
+        return threenum;
+    }
+
+    public void setThreenum(Long threenum) {
+        this.threenum = threenum;
+    }
+
+    public Long getFournum() {
+        return fournum;
+    }
+
+    public void setFournum(Long fournum) {
+        this.fournum = fournum;
+    }
+
+    public Long getFivenum() {
+        return fivenum;
+    }
+
+    public void setFivenum(Long fivenum) {
+        this.fivenum = fivenum;
+    }
+
+    public Long getSixnum() {
+        return sixnum;
+    }
+
+    public void setSixnum(Long sixnum) {
+        this.sixnum = sixnum;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+}
